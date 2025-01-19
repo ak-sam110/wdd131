90bb56aa02dcb6076c51f0e774236acadfd621db
FAB-SAM <samuelakinbola277@gmail.com>
2025-01-19 04:31:22 -0800

    week one assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set current year dynamically in footer
+document.getElementById("currentyear").textContent=new Date().getFullYear();
+
+// Set last modified date in footer
+document.getElementById("lastModified").textContent= 'Last Modified': $document.lastModified
